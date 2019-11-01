b4aae8db9d9abd4ce2942d9bcbe358e3e3274802
ntpythondev <56813095+ntpythondev@users.noreply.github.com>
2019-11-01 10:49:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as f:
+    long_desc = f.read()
 
 setuptools.setup(
     name="json2oraparser",
-    version="0.0.2",
-    author="ntpythondev",
-    author_email="nt_python_dev@gmail.com",
-    description="Parse json file to oracle database",
-    long_description=long_description,
+    version="0.1.5",
+    author="Abhishek Sinha, Samrat Ghosh, Shouvik Das, Sudipta Dey, Subhajit Bhar",
+    author_email="ntpythondev@gmail.com",
+    description="The 'json2oraparser' library parses a JSON file according to the metadata and stores Json data into Oracle database.",
+    long_description=long_desc,
     long_description_content_type="text/markdown",
+	license = "MIT",
+    platforms='any',
     url="https://github.com/ntpythondev/testpypi2019.git",
     packages=setuptools.find_packages(),
-	include_package_data=True,
-	install_requires=[
-      'cx_Oracle','pandas' 'PyYAML',
-  ],
+	package_data={'json2oraparser': ['Config/Config.yml', 'Resources/SQL/*.sql', 'METADATA/*.csv', 'METADATA/*.json', 'METADATA/*.txt']},
+	install_requires=['cx_Oracle', 'pandas', 'PyYAML'],
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-		"Development Status :: 3 - Alpha",      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-		"Intended Audience :: Developers",      # Define that your audience are developers
-		#"Topic :: Software Development :: Build Json Parser",
+	"Development Status :: 3 - Alpha"     # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+	#"Intended Audience :: Developers"    # Define that your audience are developers
+	#"Topic :: Software Development :: Build Json to Oracle Parser",
 
-    ],
+    ]
     
 )
